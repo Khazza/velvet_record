d6e88c6636fcf69cbf420d0006069340f125b290
Khazza <mahetoquet@gmail.com>
2023-05-04 11:30:02 +0200

    edit

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $count = $count_row['count'];
     <!-- Inclusion de Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <!-- Inclusion du fichier CSS personnalisé -->
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="./assets/css/styles.css">
 </head>
 
 <body>
