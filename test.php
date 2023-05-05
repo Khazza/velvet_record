840aed7f8a7fb0a07fc50d0ed6300b2e4384e49f
Khazza <mahetoquet@gmail.com>
2023-05-05 10:18:36 +0200

    kkkk

--- test.php
+++ test.php
@@ -135,4 +135,37 @@ $recent_stmt = $pdo->query($recent_sql);
 -----------------------------------------------
 
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <h5 class="card-title">Historique des 5 derniers disques ajoutés</h5>
+                    <div class="row">
+                        <?php
+                        // Requête SQL pour sélectionner les 5 derniers enregistrements de la table disc
+                        $history_sql = "SELECT * 
+                        FROM disc
+                        JOIN artist ON disc.artist_id = artist.artist_id
+                        ORDER BY disc_id DESC
+                        LIMIT 5";
+                        $history_stmt = $pdo->query($history_sql);
+
+                        // Boucle pour afficher les enregistrements de l'historique
+                        while ($history_row = $history_stmt->fetch(PDO::FETCH_ASSOC)) {
+                        ?>
+                            <div class="col-md-2">
+                                <img src="src/img/jaquettes/<?= $history_row['disc_picture']; ?>" class="card-img-top" alt="Jaquette">
+                                <p class="card-text"><span class="fw-bold">Titre: </span><?php echo $history_row['disc_title']; ?></p>
+                                <p class="card-text"><span class="fw-bold">Artiste: </span><?php echo $history_row['artist_name']; ?></p>
+                            </div>
+                        <?php
+                        }
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
