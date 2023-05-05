88ee2a749ddd78c642eaf4415e561544d3df996c
Khazza <mahetoquet@gmail.com>
2023-05-05 09:40:15 +0200

    jjjj

--- test.php
+++ test.php
@@ -31,3 +31,55 @@ $recent_stmt = $pdo->query($recent_sql);
     </div>
 </div>
 <!-- Fin de l'encadré "Ajouté récemment" -->
+
+
+------------------------------------------------------------------------------
+<!-- Encadré "Ajouté récemment" -->
+<div class="container">
+    <div class="row mt-4">
+        <div class="col">
+            <h4>Ajouté récemment :</h4>
+            <div id="recent-carousel" class="carousel slide" data-bs-ride="carousel">
+                <!-- Indicateurs -->
+                <ol class="carousel-indicators">
+                    <?php for ($i = 0; $i < $recent_count; $i++) { ?>
+                        <li data-bs-target="#recent-carousel" data-bs-slide-to="<?= $i ?>" <?php if ($i == 0) echo 'class="active"'; ?>></li>
+                    <?php } ?>
+                </ol>
+
+                <!-- Slides -->
+                <div class="carousel-inner">
+                    <?php $first = true; ?>
+                    <?php while ($recent_row = $recent_stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                        <div class="carousel-item <?php if ($first) echo 'active'; ?>">
+                            <img src="src/img/jaquettes/<?= $recent_row['disc_picture']; ?>" alt="Jaquette" class="d-block mx-auto" style="width: 200px; height: 200px; object-fit: cover;">
+                            <div class="text-center mt-2">
+                                <div><?= $recent_row['disc_title']; ?></div>
+                                <div><?= $recent_row['artist_name']; ?></div>
+                            </div>
+                        </div>
+                        <?php $first = false; ?>
+                    <?php } ?>
+                </div>
+
+                <!-- Contrôles -->
+                <a class="carousel-control-prev" href="#recent-carousel" role="button" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Précédent</span>
+                </a>
+                <a class="carousel-control-next" href="#recent-carousel" role="button" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Suivant</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Fin de l'encadré "Ajouté récemment" -->
+
+<!-- Script pour activer le carrousel -->
+<script>
+    $(document).ready(function() {
+        $('#recent-carousel').carousel();
+    });
+</script>
