630a06c60053b9cb1ac25721409320c9c9c5d3ef
Khazza <mahetoquet@gmail.com>
2023-05-05 09:58:05 +0200

    jjj

--- test.php
+++ test.php
@@ -1,44 +1,8 @@
-// Requête SQL pour sélectionner les 5 derniers disques ajoutés récemment
-$recent_sql = "SELECT * 
-               FROM disc
-               JOIN artist ON disc.artist_id = artist.artist_id
-               ORDER BY disc_id DESC
-               LIMIT 5";
-$recent_stmt = $pdo->query($recent_sql);
-
-
-
-
 <!-- Encadré "Ajouté récemment" -->
 <div class="container">
     <div class="row mt-4">
         <div class="col">
-            <h4>Ajouté récemment :</h4>
-            <div class="d-flex flex-wrap">
-                <?php while ($recent_row = $recent_stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-                    <div class="me-3 mb-3">
-                        <img src="src/img/jaquettes/<?= $recent_row['disc_picture']; ?>" alt="Jaquette" style="width: 50px; height: 50px; object-fit: cover;">
-                        <div class="text-center" style="font-size: 12px; white-space: nowrap; width: 50px; overflow: hidden; text-overflow: ellipsis;">
-                            <?= $recent_row['disc_title']; ?>
-                        </div>
-                        <div class="text-center" style="font-size: 12px; white-space: nowrap; width: 50px; overflow: hidden; text-overflow: ellipsis;">
-                            <?= $recent_row['artist_name']; ?>
-                        </div>
-                    </div>
-                <?php } ?>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- Fin de l'encadré "Ajouté récemment" -->
-
-
-------------------------------------------------------------------------------
-<!-- Encadré "Ajouté récemment" -->
-<div class="container">
-    <div class="row mt-4">
-        <div class="col">
-            <h4>Ajouté récemment :</h4>
+            <h4 class="text-center">Ajouté récemment :</h4>
             <div id="recent-carousel" class="carousel slide" data-bs-ride="carousel">
                 <!-- Indicateurs -->
                 <ol class="carousel-indicators">
@@ -48,17 +12,15 @@ $recent_stmt = $pdo->query($recent_sql);
                 </ol>
 
                 <!-- Slides -->
-                <div class="carousel-inner">
-                    <?php $first = true; ?>
+                <div class="carousel-inner d-flex justify-content-center">
                     <?php while ($recent_row = $recent_stmt->fetch(PDO::FETCH_ASSOC)) { ?>
-                        <div class="carousel-item <?php if ($first) echo 'active'; ?>">
-                            <img src="src/img/jaquettes/<?= $recent_row['disc_picture']; ?>" alt="Jaquette" class="d-block mx-auto" style="width: 200px; height: 200px; object-fit: cover;">
+                        <div class="carousel-item">
+                            <img src="src/img/jaquettes/<?= $recent_row['disc_picture']; ?>" alt="Jaquette" class="d-block mx-auto" style="width: 50px; height: 50px; object-fit: cover;">
                             <div class="text-center mt-2">
                                 <div><?= $recent_row['disc_title']; ?></div>
                                 <div><?= $recent_row['artist_name']; ?></div>
                             </div>
                         </div>
-                        <?php $first = false; ?>
                     <?php } ?>
                 </div>
 
@@ -76,10 +38,3 @@ $recent_stmt = $pdo->query($recent_sql);
     </div>
 </div>
 <!-- Fin de l'encadré "Ajouté récemment" -->
-
-<!-- Script pour activer le carrousel -->
-<script>
-    $(document).ready(function() {
-        $('#recent-carousel').carousel();
-    });
-</script>
