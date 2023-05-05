e80c07ffb7ceed3914b83f6eb2f89f60e3b7ae26
Khazza <mahetoquet@gmail.com>
2023-05-05 09:30:52 +0200

    mkmmk

--- index.php
+++ index.php
@@ -5,6 +5,15 @@ $count_sql = "SELECT COUNT(*) as count FROM disc";
 $count_stmt = $pdo->query($count_sql);
 $count_row = $count_stmt->fetch(PDO::FETCH_ASSOC);
 $count = $count_row['count'];
+
+// Requête SQL pour sélectionner les 5 derniers disques ajoutés récemment
+$recent_sql = "SELECT * 
+               FROM disc
+               JOIN artist ON disc.artist_id = artist.artist_id
+               ORDER BY disc_id DESC
+               LIMIT 5";
+$recent_stmt = $pdo->query($recent_sql);
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,7 +39,32 @@ $count = $count_row['count'];
             </div>
         </div>
     </nav>
-<div class="container">
+
+    <!-- Encadré "Ajouté récemment" -->
+    <div class="container">
+        <div class="row mt-4">
+            <div class="col">
+                <h4>Ajouté récemment :</h4>
+                <div class="d-flex flex-wrap">
+                    <?php while ($recent_row = $recent_stmt->fetch(PDO::FETCH_ASSOC)) { ?>
+                        <div class="me-3 mb-3">
+                            <img src="src/img/jaquettes/<?= $recent_row['disc_picture']; ?>" alt="Jaquette" style="width: 50px; height: 50px; object-fit: cover;">
+                            <div class="text-center" style="font-size: 12px; white-space: nowrap; width: 50px; overflow: hidden; text-overflow: ellipsis;">
+                                <?= $recent_row['disc_title']; ?>
+                            </div>
+                            <div class="text-center" style="font-size: 12px; white-space: nowrap; width: 50px; overflow: hidden; text-overflow: ellipsis;">
+                                <?= $recent_row['artist_name']; ?>
+                            </div>
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Fin de l'encadré "Ajouté récemment" -->
+
+
+    <div class="container">
         <div class="row">
             <?php
             // Requête SQL pour sélectionner tous les enregistrements de la table disc
