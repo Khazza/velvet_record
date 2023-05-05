161274578e1dcf978d61e78761a4d03fe0c6dc99
Khazza <mahetoquet@gmail.com>
2023-05-05 11:11:00 +0200

    huhuhuh

--- index.php
+++ index.php
@@ -34,9 +34,9 @@ $count = $count_row['count'];
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card mb-3">
+                <div class="card mb-3 border-0">
                     <div class="card-body">
-                        <h5 class="card-title text-center">Ajoutés récemment :</h5>
+                        <h5 class="card-title text-center" style="font-size: 18px;">Ajoutés récemment :</h5>
                         <div class="row d-flex justify-content-center">
                             <?php
                             // Requête SQL pour sélectionner les 5 derniers enregistrements de la table disc
@@ -52,8 +52,16 @@ $count = $count_row['count'];
                             ?>
                                 <div class="col-md-2 text-center">
                                     <img src="src/img/jaquettes/<?= $history_row['disc_picture']; ?>" class="card-img-top" alt="Jaquette" style="max-width: 50px;">
-                                    <p class="card-text"><span class="fw-bold">Titre: </span><?php echo $history_row['disc_title']; ?></p>
-                                    <p class="card-text"><span class="fw-bold">Artiste: </span><?php echo $history_row['artist_name']; ?></p>
+                                    <div class="mt-2">
+                                        <span class="card-text" style="font-size: 14px;">
+                                            <span class="fw-bold">Titre:</span><br>
+                                            <?php echo $history_row['disc_title']; ?>
+                                        </span><br>
+                                        <span class="card-text" style="font-size: 14px;">
+                                            <span class="fw-bold">Artiste:</span><br>
+                                            <?php echo $history_row['artist_name']; ?>
+                                        </span>
+                                    </div>
                                 </div>
                             <?php
                             }
