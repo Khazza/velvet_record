95346530080c1a001fbce6488923521680cf0972
Khazza <mahetoquet@gmail.com>
2023-05-09 15:30:45 +0200

    test

--- test.php
+++ test.php
@@ -1,1 +1,6 @@
-( ! ) Warning: Undefined array key "csrf_token" in /home/mahe/Bureau/velvet_record/signup.php on line 27 Call Stack #TimeMemoryFunctionLocation 10.0001359448{main}( ).../signup.php:0 ">
+( ! ) Warning: Undefined global variable $_SESSION in /home/mahe/Bureau/velvet_record/login.php on line 19 Call Stack #TimeMemoryFunctionLocation 10.0000356384{main}( ).../login.php:0
+( ! ) Warning: Trying to access array offset on value of type null in /home/mahe/Bureau/velvet_record/login.php on line 19
+Call Stack
+#	Time	Memory	Function	Location
+1	0.0000	356384	{main}( )	.../login.php:0
+"> 
