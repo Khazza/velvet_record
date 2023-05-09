9633e27d66d3dbab97784c97b5bb7904f7cf9bc9
Khazza <mahetoquet@gmail.com>
2023-05-09 15:16:54 +0200

    error

--- test.php
+++ test.php
@@ -1,1 +1,4 @@
-Warning: Undefined global variable $_SESSION in /home/mahe/Bureau/velvet_record/signup.php on line 23 Call Stack #TimeMemoryFunctionLocation 10.0002359440{main}( ).../signup.php:0 
+<?php
+session_start();
+include('header.php');
+?>
