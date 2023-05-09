1d7a3e8c0dcc63295eef6338616d8ed683a44a36
Khazza <mahetoquet@gmail.com>
2023-05-09 16:41:32 +0200

    112

--- test.php
+++ test.php
@@ -1,6 +1,1 @@
-( ! ) Warning: Undefined global variable $_SESSION in /home/mahe/Bureau/velvet_record/login.php on line 19 Call Stack #TimeMemoryFunctionLocation 10.0000356384{main}( ).../login.php:0
-( ! ) Warning: Trying to access array offset on value of type null in /home/mahe/Bureau/velvet_record/login.php on line 19
-Call Stack
-#	Time	Memory	Function	Location
-1	0.0000	356384	{main}( )	.../login.php:0
-"> 
+( ! ) Warning: Undefined array key "name" in /home/mahe/Bureau/velvet_record/index.php on line 8
