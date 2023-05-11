1cfb942d39992e46748bebd21cdd77372069b9ed
Khazza <mahetoquet@gmail.com>
2023-05-11 10:29:14 +0200

    signup_success

---
+++ signup_succes.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+include('db.php');
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Signup Success</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Signup Success!</h2>
+        <p>Votre compte a été créé.</p>
+        <a href="login.php" class="btn btn-primary">Log in</a>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
