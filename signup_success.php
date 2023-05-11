852b6799a05990ad13a7765aad05099266911aa6
Khazza <mahetoquet@gmail.com>
2023-05-11 15:22:06 +0200

    edit

--- signup_success.php
+++ signup_success.php
@@ -10,9 +10,9 @@ include('db.php');
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
 </head>
 <body>
-    <div class="container">
-        <h2>Signup Success!</h2>
-        <p>Votre compte a été créé.</p>
+    <div class="container text-center bg-light p-5">
+        <h2 class="text-primary">Signup Success!</h2>
+        <p class="lead">Votre compte a été créé.</p>
         <a href="login.php" class="btn btn-primary">Log in</a>
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
