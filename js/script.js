3e08a1244ae6b0006674dc2c750b12c1b53e4327
Khazza <mahetoquet@gmail.com>
2023-05-24 13:58:07 +0200

    edit

--- js/script.js
+++ js/script.js
@@ -93,55 +93,37 @@ document.getElementById('signupBtn').addEventListener('click', function(e) {
 function showLoginForm() {
     Swal.fire({
         title: 'Login',
-        html: '<input type="text" id="loginEmail" class="swal2-input" placeholder="Email">' +
-            '<input type="password" id="loginPassword" class="swal2-input" placeholder="Password">',
+        html: '<input type="text" id="loginUsername" class="swal2-input" placeholder="Username">' +
+            '<input type="password" id="loginPassword" class="swal2-input" placeholder="Password">' +
+            '<input type="hidden" id="loginCsrfToken" class="swal2-input">',
         confirmButtonText: 'Log in',
         focusConfirm: false,
         preConfirm: function() {
             return new Promise(function(resolve) {
                 resolve([
-                    document.getElementById('loginEmail').value,
-                    document.getElementById('loginPassword').value
+                    document.getElementById('loginUsername').value,
+                    document.getElementById('loginPassword').value,
+                    document.getElementById('loginCsrfToken').value
                 ]);
             });
         }
     }).then(function(result) {
-        var email = result.value[0];
+        var username = result.value[0];
         var password = result.value[1];
-        handleLogin(email, password);
+        var csrf_token = result.value[2];
+        handleLogin(username, password, csrf_token);
     });
 }
 
-function showSignupForm() {
-    Swal.fire({
-        title: 'Sign up',
-        html: '<input type="text" id="signupEmail" class="swal2-input" placeholder="Email">' +
-            '<input type="password" id="signupPassword" class="swal2-input" placeholder="Password">',
-        confirmButtonText: 'Sign up',
-        focusConfirm: false,
-        preConfirm: function() {
-            return new Promise(function(resolve) {
-                resolve([
-                    document.getElementById('signupEmail').value,
-                    document.getElementById('signupPassword').value
-                ]);
-            });
-        }
-    }).then(function(result) {
-        var email = result.value[0];
-        var password = result.value[1];
-        handleSignup(email, password);
-    });
-}
-
-function handleLogin(email, password) {
+function handleLogin(username, password, csrf_token) {
     $.ajax({
         url: 'login_handler.php',
         type: 'post',
         dataType: 'json',
         data: {
-            email: email,
-            password: password
+            username: username,
+            password: password,
+            csrf_token: csrf_token
         },
         success: function(response) {
             if (response.status === "success") {
@@ -153,14 +135,44 @@ function handleLogin(email, password) {
     });
 }
 
-function handleSignup(email, password) {
+function showSignupForm() {
+    Swal.fire({
+        title: 'Sign up',
+        html: '<input type="text" id="signupUsername" class="swal2-input" placeholder="Username">' +
+            '<input type="password" id="signupPassword" class="swal2-input" placeholder="Password">' +
+            '<input type="password" id="signupConfirmPassword" class="swal2-input" placeholder="Confirm Password">' +
+            '<input type="hidden" id="signupCsrfToken" class="swal2-input">',
+        confirmButtonText: 'Sign up',
+        focusConfirm: false,
+        preConfirm: function() {
+            return new Promise(function(resolve) {
+                resolve([
+                    document.getElementById('signupUsername').value,
+                    document.getElementById('signupPassword').value,
+                    document.getElementById('signupConfirmPassword').value,
+                    document.getElementById('signupCsrfToken').value
+                ]);
+            });
+        }
+    }).then(function(result) {
+        var username = result.value[0];
+        var password = result.value[1];
+        var confirmPassword = result.value[2];
+        var csrf_token = result.value[3];
+        handleSignup(username, password, confirmPassword, csrf_token);
+    });
+}
+
+function handleSignup(username, password, confirmPassword, csrf_token) {
     $.ajax({
         url: 'signup_handler.php',
         type: 'post',
         dataType: 'json',
         data: {
-            email: email,
-            password: password
+            username: username,
+            password: password,
+            confirm_password: confirmPassword,
+            csrf_token: csrf_token
         },
         success: function(response) {
             if (response.status === "success") {
