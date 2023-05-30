eb4e4408ecc05cb6ed8517e5a7d0e96ebde3d3b9
Khazza <mahetoquet@gmail.com>
2023-05-30 11:15:49 +0200

    edit

--- login.php
+++ login.php
@@ -22,7 +22,9 @@ if (isset($_GET['error'])) {
 <html>
 <head>
     <title>Login</title>
+    <!-- Inclusion de Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <!-- Inclusion du fichier CSS personnalisé -->
     <link rel="stylesheet" href="./assets/css/styles.css">
 </head>
@@ -44,14 +46,14 @@ if (isset($_GET['error'])) {
                         <input type="text" class="form-control" name="username" required>
                     </div>
                     <div class="mb-3">
-    <label for="password" class="form-label">Password:</label>
-    <div class="input-group">
-        <input type="password" class="form-control" name="password" id="password" required>
-        <button type="button" id="togglePassword" class="btn btn-outline-secondary">
-            <i class="fas fa-eye"></i>
-        </button>
-    </div>
-</div>
+                        <label for="password" class="form-label">Password:</label>
+                        <div class="input-group">
+                            <input type="password" class="form-control" name="password" id="password" required>
+                            <button type="button" id="togglePassword" class="btn btn-outline-secondary">
+                                <i class="fas fa-eye"></i>
+                            </button>
+                        </div>
+                    </div>
                     <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary">Login</button>
@@ -60,6 +62,13 @@ if (isset($_GET['error'])) {
             </div>
         </div>
     </div>
+    <!-- Inclusion de jQuery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <!-- Inclusion des scripts Bootstrap -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/js/bootstrap.bundle.min.js"></script>
+    <!-- Inclusion de SweetAlert2 -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.5/dist/sweetalert2.all.min.js"></script>
+    <!-- Inclusion du script JS personnalisé -->
+    <script src="./js/script.js"></script>
 </body>
 </html>
