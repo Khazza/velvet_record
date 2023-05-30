a6802c40c3b7236b3574c2daceb2a02e3b69bc4a
Khazza <mahetoquet@gmail.com>
2023-05-30 13:25:22 +0200

    test

--- js/script.js
+++ js/script.js
@@ -156,19 +156,3 @@ $(document).ready(function() {
         });
     });
 });
-
-
-
-// -----------------------------Password
-const passwordInput = document.getElementById('password');
-const togglePasswordButton = document.getElementById('togglePassword');
-
-togglePasswordButton.addEventListener('click', function () {
-    if (passwordInput.type === 'password') {
-        passwordInput.type = 'text';
-        togglePasswordButton.innerHTML = '<i class="fas fa-eye-slash"></i>';
-    } else {
-        passwordInput.type = 'password';
-        togglePasswordButton.innerHTML = '<i class="fas fa-eye"></i>';
-    }
-});
