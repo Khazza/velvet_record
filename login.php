bfe6c6b6f02ebd66b506ac5572bc305d3b9a0d8c
Khazza <mahetoquet@gmail.com>
2023-05-30 11:10:20 +0200

    edit password hide

--- login.php
+++ login.php
@@ -44,9 +44,14 @@ if (isset($_GET['error'])) {
                         <input type="text" class="form-control" name="username" required>
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password:</label>
-                        <input type="password" class="form-control" name="password" required>
-                    </div>
+    <label for="password" class="form-label">Password:</label>
+    <div class="input-group">
+        <input type="password" class="form-control" name="password" id="password" required>
+        <button type="button" id="togglePassword" class="btn btn-outline-secondary">
+            <i class="fas fa-eye"></i>
+        </button>
+    </div>
+</div>
                     <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
                     <div class="text-center">
                         <button type="submit" class="btn btn-primary">Login</button>
