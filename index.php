d9025481472b4ead0bd1c92dd1649366fcc34dc9
Khazza <mahetoquet@gmail.com>
2023-05-30 09:37:07 +0200

    edit

--- index.php
+++ index.php
@@ -33,10 +33,7 @@ $count = $count_row['count'];
     <nav class="navbar navbar-expand-lg navbar-light bg-light py-0">
         <div class="container">
             <a class="navbar-brand fs-1 fw-bold">
-                Liste des disques
-                <div class="info-bubble">
-                    <?php echo $count; ?>
-                </div>
+                Liste des disques <span class="badge bg-secondary"><?php echo $count; ?></span>
             </a>
             <div class="ml-auto">
                 <?php if (isset($_SESSION['user'])) { ?>
