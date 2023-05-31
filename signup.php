5cab69d8e8868fb2aba560fdf26e6d998ce5cf99
Khazza <mahetoquet@gmail.com>
2023-05-31 11:01:40 +0200

    edit

--- signup.php
+++ signup.php
@@ -57,9 +57,6 @@ $errors = isset($_SESSION['errors']) ? $_SESSION['errors'] : [];
                     <span>Already have an account? <a href="login.php" class="text-login">Login</a></span>
                 </div>
                 <div class="text-center mt-2">
-                    <span>Or go back to <a href="index.php">Home</a></span>
-                </div>
-                <div class="text-center mt-2">
                     <span>Or go back to <a href="index.php" class="back-index">Home</a></span>
                 </div>
             </div>
